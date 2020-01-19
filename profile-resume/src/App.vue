c06e6b52907e5dae37dacdb60bd77dbdd8d92dce
Mie Chayakorn <miechayakorn@gmail.com>
2020-01-19 14:14:05 +0700

    [Add] profile resume

--- profile-resume/src/App.vue
+++ profile-resume/src/App.vue
@@ -1,32 +1,425 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="container">
+      <h3 class="animated fadeInUp">Profile</h3>
+      <article class="col-md-12">
+        <div class="container">
+          <div class="row d-flex justify-content-center">
+            <div class="col-md-5">
+              <div class="card card-profile animated fadeInUp">
+                <a
+                  style="position: absolute;
+              right: 0;
+              top: 0;"
+                  class="p-2"
+                  href="https://www.miechayakorn.tk/pic/miechayakorn.jpg"
+                >
+                  Resume
+                </a>
+                <div class="card-avatar">
+                  <img class="img" src="https://www.miechayakorn.tk/pic/miechayakorn.jpg" />
+                </div>
+
+                <div class="table">
+                  <h4 class="card-caption">Chayakorn Pradchayakul</h4>
+                  <h6 class="card-description text-muted">
+                    I am a beginner developer 🧸
+                  </h6>
+                  <p class="card-description">
+                    King Mongkut's University of Technology Thonburi <br />
+                    ===== <br />
+                    <i class="fa fa-map-marker ">
+                      40/224 Rama II Rd, Bang Mot, Chom Thong, Bangkok 10150</i
+                    >
+                    <br />
+                  </p>
+                  <div class="ftr">
+                    <a
+                      href="https://fb.com/MieChayakorn1"
+                      class="btn btn-just-icon btn-facebook btn-round"
+                      target="_blank"
+                    >
+                      <i class="fa fa-facebook"></i>
+                    </a>
+                    <a
+                      href="https://line.me/ti/p/VHm8IGfVBS"
+                      style="background-color: #00b900;"
+                      class="btn btn-just-icon btn-round"
+                      target="_blank"
+                    >
+                      <img
+                        src="https://www.miechayakorn.tk/pic/LINE_APP.png"
+                        style="width: 20px;"
+                        alt=""
+                      />
+                    </a>
+                    <a
+                      href="https://instagram.com/mie_chayakorn"
+                      style="background-color: #c13584; color: #f5f5f5;"
+                      class="btn btn-just-icon btn-instagram btn-round"
+                      target="_blank"
+                    >
+                      <i class="fa fa-instagram"></i>
+                    </a>
+                    <a
+                      href="https://github.com/miechayakorn"
+                      style="background-color: #554488; color: #f5f5f5;"
+                      class="btn btn-just-icon btn-github btn-round"
+                      target="_blank"
+                    >
+                      <i class="fa fa-github"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </article>
     </div>
-    <router-view/>
+
+    <div
+      class="section p-1 mb-4 animated fadeInLeftBig delay-1s"
+      style="background-color: #3D3731;"
+    >
+      <div class="container">
+        <h3>Project</h3>
+      </div>
+    </div>
+
+    <div class="container">
+      <article class="col-md-12">
+        <div class="row">
+          <div class="container">
+            <div class="card-deck mb-4">
+              <div class="card card-blog animated fadeInUp delay-2s">
+                <div class="card-image">
+                  <a href="#"> <img class="img" src="https://www.miechayakorn.tk/pic/Fruity.PNG" /> </a>
+                  <div class="ripple-cont"></div>
+                </div>
+                <div class="table">
+                  <h6 class="category text-success">
+                    <i class="fa fa-university"></i> INT106 WEB technology
+                  </h6>
+                  <h4 class="card-caption">
+                    Fruity shop
+                  </h4>
+                  <p class="card-description">
+                    โปรเจครายวิชา INT106 WEB technology เป็นวิชาที่ฝึกเขียน html
+                    ร่วมกับ javascript
+                  </p>
+                  <div class="col text-center">
+                    <a href="https://pro017.miechayakorn.tk/" target="_blank">
+                      <button
+                        class="btn btn-github"
+                        style="background-color: #554488; color: #f5f5f5;"
+                      >
+                        View WEB
+                      </button>
+                    </a>
+                  </div>
+                  <div class="ftr">
+                    <div class="author">
+                      <p class="card-text">
+                        <small class="text-muted"
+                          >Project - 1st year student</small
+                        >
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="card card-blog animated fadeInUp delay-2s">
+                <div class="card-image">
+                  <a href="#"> <img class="img" src="https://www.miechayakorn.tk/pic/BBM.PNG" /> </a>
+                  <div class="ripple-cont"></div>
+                </div>
+                <div class="table">
+                  <h6 class="category text-success">
+                    <i class="fa fa-university"></i> INT.....
+                  </h6>
+                  <h4 class="card-caption">
+                    BBMProject
+                  </h4>
+                  <p class="card-description">
+                    โปรเจคเว็บ Condominium - เป็น Java web app
+                    ไว้สำหรับจองซื้อห้องของ Condominium เป็นโปรเจคของรายวิชา
+                    Java Web App. และ ใช้ DB ของ derby ซึ่งใช้อยู่ในโปรแกรม
+                    NetBeans
+                  </p>
+                  <div class="col text-center">
+                    <a
+                      href="https://github.com/miechayakorn/BBMProject"
+                      target="_blank"
+                    >
+                      <button
+                        class="btn btn-github"
+                        style="background-color: #554488; color: #f5f5f5;"
+                      >
+                        View Github
+                      </button>
+                    </a>
+                  </div>
+                  <div class="ftr">
+                    <div class="author">
+                      <p class="card-text">
+                        <small class="text-muted"
+                          >Project - 2nd year student</small
+                        >
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="card card-blog animated fadeInUp delay-2s">
+                <div class="card-image">
+                  <a href="#">
+                    <img class="img" src="https://placehold.it/500x270" />
+                  </a>
+                  <div class="ripple-cont"></div>
+                </div>
+                <div class="table">
+                  <h6 class="category text-success">
+                    <i class="fa fa-university"></i> INT.....
+                  </h6>
+                  <h4 class="card-caption">
+                    TEXT
+                  </h4>
+                  <p class="card-description">description</p>
+                  <div class="col text-center">
+                    <a
+                      href="https://github.com/miechayakorn/BBMProject"
+                      target="_blank"
+                    >
+                      <button
+                        class="btn btn-github"
+                        style="background-color: #554488; color: #f5f5f5;"
+                      >
+                        View Github
+                      </button>
+                    </a>
+                  </div>
+                  <div class="ftr">
+                    <div class="author">
+                      <p class="card-text">
+                        <small class="text-muted"
+                          >Project - 2nd year student</small
+                        >
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="card-deck mb-4">
+              <div class="card card-blog animated fadeInUp delay-2s">
+                <div class="card-image">
+                  <a href="#">
+                    <img class="img" src="https://placehold.it/500x270" />
+                  </a>
+                  <div class="ripple-cont"></div>
+                </div>
+                <div class="table">
+                  <h6 class="category text-success">
+                    <i class="fa fa-university"></i> INT.....
+                  </h6>
+                  <h4 class="card-caption">
+                    TEXT
+                  </h4>
+                  <p class="card-description">description</p>
+                  <div class="col text-center">
+                    <a
+                      href="https://github.com/miechayakorn/BBMProject"
+                      target="_blank"
+                    >
+                      <button
+                        class="btn btn-github"
+                        style="background-color: #554488; color: #f5f5f5;"
+                      >
+                        View Github
+                      </button>
+                    </a>
+                  </div>
+                  <div class="ftr">
+                    <div class="author">
+                      <p class="card-text">
+                        <small class="text-muted"
+                          >Project - 2nd year student</small
+                        >
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="card card-blog animated fadeInUp delay-2s">
+                <div class="card-image">
+                  <a href="#">
+                    <img class="img" src="https://placehold.it/500x270" />
+                  </a>
+                  <div class="ripple-cont"></div>
+                </div>
+                <div class="table">
+                  <h6 class="category text-success">
+                    <i class="fa fa-university"></i> INT.....
+                  </h6>
+                  <h4 class="card-caption">
+                    TEXT
+                  </h4>
+                  <p class="card-description">description</p>
+                  <div class="col text-center">
+                    <a
+                      href="https://github.com/miechayakorn/BBMProject"
+                      target="_blank"
+                    >
+                      <button
+                        class="btn btn-github"
+                        style="background-color: #554488; color: #f5f5f5;"
+                      >
+                        View Github
+                      </button>
+                    </a>
+                  </div>
+                  <div class="ftr">
+                    <div class="author">
+                      <p class="card-text">
+                        <small class="text-muted"
+                          >Project - 2nd year student</small
+                        >
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="card card-blog animated fadeInUp delay-2s">
+                <div class="card-image">
+                  <a href="#">
+                    <img class="img" src="https://placehold.it/500x270" />
+                  </a>
+                  <div class="ripple-cont"></div>
+                </div>
+                <div class="table">
+                  <h6 class="category text-success">
+                    <i class="fa fa-university"></i> INT.....
+                  </h6>
+                  <h4 class="card-caption">
+                    TEXT
+                  </h4>
+                  <p class="card-description">description</p>
+                  <div class="col text-center">
+                    <a
+                      href="https://github.com/miechayakorn/BBMProject"
+                      target="_blank"
+                    >
+                      <button
+                        class="btn btn-github"
+                        style="background-color: #554488; color: #f5f5f5;"
+                      >
+                        View Github
+                      </button>
+                    </a>
+                  </div>
+                  <div class="ftr">
+                    <div class="author">
+                      <p class="card-text">
+                        <small class="text-muted"
+                          >Project - 2nd year student</small
+                        >
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </article>
+    </div>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  /* background: #1e2327; */
+  background: linear-gradient(
+    to left bottom,
+    #ffd168,
+    #ffc369,
+    #ffb56d,
+    #ffa973,
+    #ff9d79,
+    #fc937f,
+    #f88b85,
+    #f1838b,
+    #e47c90,
+    #d67694,
+    #c67196,
+    #b56d97
+  );
+  font-family: "Exo", sans-serif;
+  color: #fff;
+  /* font-weight: 200; */
+}
+
+h1,
+h2,
+h3 {
+  font-family: "Exo", sans-serif;
+  text-transform: uppercase;
+  font-weight: 200;
+}
+
+footer {
+  background-color: #dfe0df;
+  position: fixed;
+  height: 60px;
+  bottom: 0;
+  width: 100%;
+}
+
+/* --------------------------------------------------------------- */
+
+.sidenav {
+  height: 100%;
+  width: 0;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  background-color: #111;
+  overflow-x: hidden;
+  transition: 0.5s;
+  padding-top: 60px;
   text-align: center;
-  color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+.sidenav a {
+  padding: 8px 8px 8px 32px;
+  text-decoration: none;
+  font-size: 25px;
+  color: #818181;
+  display: block;
+  transition: 0.3s;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.sidenav a:hover {
+  color: #f1f1f1;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.sidenav .closebtn {
+  position: absolute;
+  top: 0;
+  right: 25px;
+  font-size: 36px;
+  margin-left: 50px;
+}
+
+@media screen and (max-height: 450px) {
+  .sidenav {
+    padding-top: 15px;
+  }
+
+  .sidenav a {
+    font-size: 18px;
+  }
 }
 </style>
