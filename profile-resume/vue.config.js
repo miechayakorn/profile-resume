f392ce7d2d4148126dc3c1ace75a219d637c4d2b
Mie Chayakorn <miechayakorn@gmail.com>
2020-11-18 22:51:48 +0700

    [Add] clear cache

---
+++ profile-resume/vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    pwa: {
+      name: 'Profile - Chayakorn Pradchayakul',
+      themeColor: '#3d3731',
+      msTileColor: '#777777',
+      appleMobileWebAppCapable: 'yes',
+      appleMobileWebAppStatusBarStyle: 'black',
+      workboxOptions: {
+        skipWaiting: true
+      }
+    },
+  }
